d34feaf1a417ba4b31543555befcdf43cf562cb4
badri95699 <badri.gupta.work@gmail.com>
2026-07-24 12:48:03 +0530

    feat:login,logout

---
+++ src/components/Body.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import { Outlet, useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status == 401) {
+        navigate("/login");
+      } else {
+        // can make a error page here 
+        // if some other type of error came
+        console.log(err);
+      }
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  },[]);
+
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+const Feed = () => {
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate(); 
+
+  useEffect(() => {
+    if(!user) {
+      navigate("/login");
+    }
+  }, []);
+
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
